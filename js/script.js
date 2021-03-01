0015d8d8e4bf5c7665f75e8543e224bd3f01daf2
Dmytro-Len <msalmanac@gmail.com>
2021-03-02 00:37:13 +0200

    added slider

--- js/script.js
+++ js/script.js
@@ -2,10 +2,42 @@ $('.slider').slick({
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 2,
-    centerMode: true,
+    slidesToShow: 3,
+    centerMode: false,
     variableWidth: true,
-    appendDots: '#mainSlider__dots1',
+    // appendDots: '#mainSlider__dots1',
     prevArrow: $('.left-tick'),
-    nextArrow: $('.right-tick')
+    nextArrow: $('.right-tick'),
+    responsive: [
+      {
+        breakpoint: 698,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 375,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        }
+      },
+    ]
   });
+
+  $('.header__slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: false,
+    variableWidth: true,
+    // appendDots: '#mainSlider__dots1',
+    prevArrow: $('.left-tick'),
+    nextArrow: $('.right-tick'),
+  })
